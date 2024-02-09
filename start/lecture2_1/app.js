7b221c0366773d51cb7d744ea06d1ee8fc3f22e7
omen <barkeromen@gmail.com>
2024-02-10 01:34:36 +0700

    projekx

--- start/lecture2_1/app.js
+++ start/lecture2_1/app.js
@@ -26,7 +26,7 @@ class App{
 		
         const geometry = new THREE.TorusKnotBufferGeometry( 0.8, 0.3, 120, 16 ); 
         
-        const material = new THREE.MeshBasicMaterial( { color: 0xFF1111 });
+        const material = new THREE.MeshBasicMaterial( { color: 0x000000 });
 
         this.mesh = new THREE.Mesh( geometry, material );
         
